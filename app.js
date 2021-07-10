b7be3fd933c652e061097b835877f832609e2c04
Mudasir Qazi <mudasirqazi.dev@gmail.com>
2021-07-10 14:20:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const express = require('express');
+const etsyjs = require('etsy-js');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const url = require('url');
+const app = express();
+
+// axios.get(`https://www.etsy.com/oauth/connect?
+// response_type=code
+// &redirect_uri=http://localhost:3000/authorise
+// &scope=transactions_r%20transactions_w
+// &client_id=hdqn6kwt4c8n5aps1rsy0a0p
+// &state=superstate
+// &code_challenge=DSWlW2Abh-cf8CeLL8-g3hQ2WQyYdKyiu83u_s7nRhI
+// &code_challenge_method=S256`);
+
+app.get('/', async (req, res) => {
+    // let response = await axios.get(`https://www.etsy.com/oauth/connect?
+    //     response_type=code
+    //     &redirect_uri=http://localhost:3000/authorise
+    //     &scope=transactions_r%20transactions_w
+    //     &client_id=hdqn6kwt4c8n5aps1rsy0a0p
+    //     &state=superstate
+    //     &code_challenge=DSWlW2Abh-cf8CeLL8-g3hQ2WQyYdKyiu83u_s7nRhI
+    //     &code_challenge_method=S256`);
+    res.redirect(`https://www.etsy.com/oauth/connect?
+    response_type=code
+    &redirect_uri=https://tolocalhost.com:3000/authorise
+    &scope=transactions_r%20transactions_w
+    &client_id=hdqn6kwt4c8n5aps1rsy0a0p
+    &state=superstate
+    &code_challenge=DSWlW2Abh-cf8CeLL8-g3hQ2WQyYdKyiu83u_s7nRhI
+    &code_challenge_method=S256`);
+});
+
+
+app.get('/authorise', (req, res) => {
+    console.log("authorise");
+    let query = url.parse(req.url, true).query;
+    console.log(query);
+    // let verifier = query.oauth_verifier;
+    // client.accessToken(req.session.token, req.session.sec, verifier, (err, response) => {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log("accessToken: ", response);
+    //     req.session.token = response.token;
+    //     req.session.sec = response.tokenSecret;
+    //     res.redirect('/find');
+    // });
+});
+
+app.listen(3000, () => {
+    console.log("Server started @3000");
+});
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const etsyjs = require('etsy-js');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const url = require('url');
+
+const app = express();
+app.use(cookieParser('secEtsy'));
+app.use(session());
+
+const client = etsyjs.client({
+    key: 'hdqn6kwt4c8n5aps1rsy0a0p',
+    secret: 'vtt0qazg96',
+    callbackURL: 'http://localhost:3000/authorise'
+});
+
+app.get('/', (req, res) => {
+    client.requestToken((err, response) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("requestToken: ", response);
+        req.session.token = response.token;
+        req.session.sec = response.tokenSecret;
+        res.redirect(response.loginUrl);
+    });
+});
+
+app.get('/authorise', (req, res) => {
+    console.log("authorise");
+    let query = url.parse(req.url, true).query;
+    let verifier = query.oauth_verifier;
+    client.accessToken(req.session.token, req.session.sec, verifier, (err, response) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("accessToken: ", response);
+        req.session.token = response.token;
+        req.session.sec = response.tokenSecret;
+        res.redirect('/find');
+    });
+});
+
+app.get('/find', (req, res) => {
+    client.auth(req.session.token, req.session.sec).user("etsyjs").find((err, body, headers) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(body.results[0]);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server started @3000");
+});
